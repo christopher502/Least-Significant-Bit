458e7fd6fb4359689ef32cc6d8c327a0dcc61016
christopher502 <chihaicristian.contact@gmail.com>
2024-01-20 12:24:17 +0200

    initial commit

---
+++ src/test/java/com/steganography/LeastSignificantBit/LeastSignificantBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steganography.LeastSignificantBit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeastSignificantBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
