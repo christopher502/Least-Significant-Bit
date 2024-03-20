c9d9c807bfe8180ffd1b2bf48868db4912c8340b
christopher502 <chihaicristian.contact@gmail.com>
2024-03-20 16:00:00 +0200

    Fixed svc typo

--- frontend/generated/flow/generated-flow-imports.js
+++ frontend/generated/flow/generated-flow-imports.js
@@ -9,11 +9,11 @@ import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
 
 const loadOnDemand = (key) => {
   const pending = [];
-  if (key === '36eec818a944aaae7420b447e171d7b9997e2f766be8026597ce14e4aae29a8d') {
-    pending.push(import('./chunks/chunk-068624e1cb60d5543629fa2af62c2790667a7f37fbb2b02f6add7f6fb34b0641.js'));
-  }
   if (key === 'cd4924141ba10284aa940fdacb24a08b6c564f3687c4afd3212686bac2b91890') {
     pending.push(import('./chunks/chunk-25987efe5632fff434e05c531e9e427ac422df28f1561a0981f0e4df82a821cd.js'));
+  }
+  if (key === '36eec818a944aaae7420b447e171d7b9997e2f766be8026597ce14e4aae29a8d') {
+    pending.push(import('./chunks/chunk-068624e1cb60d5543629fa2af62c2790667a7f37fbb2b02f6add7f6fb34b0641.js'));
   }
   return Promise.all(pending);
 }
